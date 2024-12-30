2ea740fc61caa28d760944c954f986821bf51acb
iMahimaSingh <155384344+iMahimaSingh@users.noreply.github.com>
2024-12-30 23:31:33 +0530

    add 26 and 189 leetcode array question

--- coderun.py
+++ coderun.py
@@ -281,6 +281,54 @@ class Solution:
         # Array is a rotated sorted array if the order is violated at most once
         return count <= 1
 
+#26. Remove Duplicates from Sorted Array
+#Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+#Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+#Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+#Return k.
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        # Pointer for the unique elements
+        unique_index = 0
+        
+        for i in range(1, len(nums)):
+            # If the current element is different from the last unique element
+            if nums[i] != nums[unique_index]:
+                unique_index += 1
+                nums[unique_index] = nums[i]
+        
+        # Return the count of unique elements
+        return unique_index + 1
+
+
+#189. Rotate Array
+#Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n  # Handle cases where k >= n
+        
+        # Reverse the entire array
+        nums.reverse()
+        
+        # Reverse the first k elements
+        nums[:k] = reversed(nums[:k])
+        
+        # Reverse the rest of the elements
+        nums[k:] = reversed(nums[k:])
+
+
+
 
       
 
